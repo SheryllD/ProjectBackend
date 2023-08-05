1891b44a1457592089dc7d5bb3856229f9d711f2
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-08-05 20:13:22 +0200

    added some notes in index routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,2 +1,10 @@
-//const express = require ('express')
-//const router = express.Router()
+const router = require("express").Router();
+const User = require("../models/User.model"); 
+
+router.get("/", (req, res, next) => {   //update this route
+const response = await User.find()
+
+  res.json("All good in here");
+});
+
+module.exports = router;
