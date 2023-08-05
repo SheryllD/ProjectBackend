42f01b118f8f00296d24c12577c47e355e8dbeba
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-08-05 14:45:28 +0200

    added new branch

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
-const express = require('express')
+const express = require('express') //maybe I have to delete this
+//const mongoose = require("mongoose")
 
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5005
 const PORT = process.env.PORT || 5005;
