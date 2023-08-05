ea9224ad0640337071166446242d99f8300eca7d
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-08-06 00:48:47 +0200

    worked on user routes, all ok

--- routes/ToDos.routes.js
+++ routes/ToDos.routes.js
@@ -1,12 +1,15 @@
 const router = require("express").Router();
 const TodoItem = require("../models/ToDos.model"); 
 
-router.get("/", (req, res, next) => {   //update this route
+router.get("/", async (req, res, next) => {  
+    const response = await TodoItem.find() 
   res.json("To Do is running ok");
 });
 
 //GET to get on TodoItem
-
+router.get('/', async (req, res) => {
+    const response = await TodoItem.findById()
+})
 //TO create one TodoItem
 
 //PUT to update one TodoItem
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,16 +1,38 @@
 const router = require("express").Router();
+const { response } = require("express");
 const User = require("../models/User.model"); 
 
-router.get("/", (req, res, next) => {   //update this route
-//const response = await User.find()
+router.get("/", async (req, res, next) => {   //update this route
+const response = await User.find()
   res.json("Everything is running ok");
 });
 
 // get to get a user 
+router.get('/:userId', async (req, res) => {
+  console.log(req.params)
+  const response = await User.findById(req.params.userId)
+
+  res.json(response)
+})
 
 //post to create a user 
+router.post('/', async (req, res) => {
+  const oneUser = await User.create(req.body)
+  res.json(201).json(oneUser)
+})
 
 //put to update a user 
+router.put('/:userId', async (req, res) => {
+  const {userId} = req.params;
 
-//delete 
+const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true })
+res.json(response)
+})
+
+//DELETE to delete one user
+router.delete('/:userId', async (req, res) => {
+  const deletedUser = await User.findByIdAndDelete(req.params.userId)
+
+res.status(202).json({message: "User successfully deleted"})
+})
 module.exports = router;
