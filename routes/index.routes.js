6984b57daa01098b3e6534d13dd46cab178ce02e
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-08-05 20:16:32 +0200

    fixed errors

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,35 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require ("./user.routes") 
 
-// get signup
-router.get("/signup", (req, res, next) => {
-  res.json("sign-up ok");
+// get homepage
+router.get("/", (req, res, next) => {
+  res.json("all good");
 });
 
-//router.use("/user/routes", userRoutes)
-
-// post signup
-
-//validation 
-
-//check if this email is already being used
-
-//create new user in the database
-
-//check if user exist
-
-//check if password is correct
-
-// sign the jwt + add algorithm
-
-//send token to the front 
-
-//incorrect password should get code 401
-
-// if no user, also error 401
-
-// //if problem with the server then error 500
-
-// Get Rout to verify the token 
 
 module.exports = router;
